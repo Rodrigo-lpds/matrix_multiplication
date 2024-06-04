2e4f3df5551e684373998a8387dd04fce1ae73bb
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2024-06-04 10:38:34 -0300

    removendo comentários desnecessários

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -3,8 +3,8 @@
 from decimal import Decimal
 import timeit
 import random
-begins = timeit.default_timer()
 
+begins = timeit.default_timer()
 
 def multiply(m_0,m_1):
     n = len(m_0)
@@ -32,21 +32,10 @@ Q = []
 
 q = 512 #Valores : 32, 64, 128, 256, 512
 
-start = timeit.default_timer()
 P = build(q, q)
-end = timeit.default_timer()
-print('A matrix P levou: %f segundos para ser criada' % (end - start))
-
-start = timeit.default_timer()
 Q = build(q, q)
-end = timeit.default_timer()
-print('A matrix Q levou: %f segundos para ser criada' % (end - start))
-
-start = timeit.default_timer()
 result = multiply(P, Q)
-end = timeit.default_timer()
 
-print('A matrix M levou: %f segundos para ser criada' % (end - start))
 end_progr = timeit.default_timer()
-print('O tempo de execução do programa foi de: %f segundos ' % (end_progr -
+print('O tempo de execução do programa para uma matrix %i x %i foi de: %f segundos ' % (q,q,end_progr -
 begins))
